d1b3255da7f6691b0f2ecc38a0cdc8afd1e7d8e5
Posado-Sebastian <101263239+Posado-Sebastian@users.noreply.github.com>
2023-03-08 13:18:22 -0300

    Add files via upload

--- Posado_Labo/src/unindad_0/ReemplazoLetra2.java
+++ Posado_Labo/src/unindad_0/ReemplazoLetra2.java
@@ -1,4 +1,14 @@
 package unindad_0;
 
+import java.util.Scanner;
+
 public class ReemplazoLetra2 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese una letra para sustituir por la e en la frase");
+        String letraString = entrada.nextLine();
+        String frase = "Ayer me compré muñecos de la marca ‘Colchitas’ por internet";
+        frase = frase.replaceAll("e",letraString);
+        System.out.println(frase.replaceAll("é",letraString));
+    }
 }
